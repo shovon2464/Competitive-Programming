7faf0b2ca4eed5cd41810becf66f3914b02b5db5
Shovon Bhowmick <shovon2464@gmail.com>
2022-04-15 14:04:48 +0600

    Vector data structure has been practiced

---
+++ STL/vector.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+    vector<int> v;
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int no;
+        cin>>no;
+        v.push_back(no);
+        cout<<"Capacity: ";
+    }
+
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    
+
+
+
+
+
+
+
+
+    return 0;
+}
